c740e9d07f39af608dd2b0c2890eccbdb87049a1
Andreas Kurtz <mail@andreas-kurtz.de>
2017-10-24 15:31:29 +0200

    convert indentation to spaces

--- main.mm
+++ main.mm
@@ -53,63 +53,63 @@ static NSArray* apps;
 
 - (void) searchApp:(NSString *)searchTerm
 {
-	NSPredicate *predicateExecutable = [NSPredicate predicateWithFormat:@"bundleExecutable contains[cd] %@", searchTerm];
-  	NSPredicate *predicateIdentifier = [NSPredicate predicateWithFormat:@"bundleIdentifier contains[cd] %@", searchTerm];
-	NSPredicate *nameIdentifier = [NSPredicate predicateWithFormat:@"localizedName contains[cd] %@", searchTerm];
-	NSArray *subPredicates = [NSArray arrayWithObjects:predicateExecutable, predicateIdentifier, nameIdentifier, nil];
-	NSCompoundPredicate *predicate = [NSCompoundPredicate orPredicateWithSubpredicates:subPredicates];
+    NSPredicate *predicateExecutable = [NSPredicate predicateWithFormat:@"bundleExecutable contains[cd] %@", searchTerm];
+    NSPredicate *predicateIdentifier = [NSPredicate predicateWithFormat:@"bundleIdentifier contains[cd] %@", searchTerm];
+    NSPredicate *nameIdentifier = [NSPredicate predicateWithFormat:@"localizedName contains[cd] %@", searchTerm];
+    NSArray *subPredicates = [NSArray arrayWithObjects:predicateExecutable, predicateIdentifier, nameIdentifier, nil];
+    NSCompoundPredicate *predicate = [NSCompoundPredicate orPredicateWithSubpredicates:subPredicates];
 
-	NSArray *filtered = [apps filteredArrayUsingPredicate:predicate];
+    NSArray *filtered = [apps filteredArrayUsingPredicate:predicate];
 
-	int i = 1;
+    int i = 1;
 
-	for (LSApplicationProxy *app in filtered)
-	{
-		NSString *teamID = app.teamID;
-	    NSString *identifier = app.bundleIdentifier;
-	    NSString *bundleContainer = app.bundleContainerURL.path;
+    for (LSApplicationProxy *app in filtered)
+    {
+        NSString *teamID = app.teamID;
+        NSString *identifier = app.bundleIdentifier;
+        NSString *bundleContainer = app.bundleContainerURL.path;
 
-	    NSString *dataContainer = app.dataContainerURL.path;
-	    NSString *localizedName = app.localizedName;
-	    NSDictionary *groupContainerURLs = app.groupContainerURLs;
+        NSString *dataContainer = app.dataContainerURL.path;
+        NSString *localizedName = app.localizedName;
+        NSDictionary *groupContainerURLs = app.groupContainerURLs;
 
-	    if(bundleContainer == nil)
-	    	bundleContainer = app.bundleURL.path;
+        if(bundleContainer == nil)
+            bundleContainer = app.bundleURL.path;
 
-	    if(![teamID isEqualToString:@"0000000000"])
-	    	identifier = [NSString stringWithFormat:@"%@.%@", teamID, identifier];
+        if(![teamID isEqualToString:@"0000000000"])
+            identifier = [NSString stringWithFormat:@"%@.%@", teamID, identifier];
 
-	    NSLog(@"[%i] %@ (%@)", i, localizedName, identifier);
+        NSLog(@"[%i] %@ (%@)", i, localizedName, identifier);
 
-	    if(bundleContainer)
-	    	NSLog(@"Bundle: %@", bundleContainer);
+        if(bundleContainer)
+            NSLog(@"Bundle: %@", bundleContainer);
 
-	    if(dataContainer)
-	    	NSLog(@"Data: %@", dataContainer);
+        if(dataContainer)
+            NSLog(@"Data: %@", dataContainer);
 
-	    if(groupContainerURLs != nil && [groupContainerURLs count] > 0) {
-	    	for (id key in groupContainerURLs ) {
-	    		NSURL *groupURL = [groupContainerURLs objectForKey:key];
-	    		NSLog(@"Group: %@ (%@)\n", groupURL.path, key);
-	    	}
-	    } else {
-	    	NSLog(@"");
-	    }
+        if(groupContainerURLs != nil && [groupContainerURLs count] > 0) {
+            for (id key in groupContainerURLs ) {
+                NSURL *groupURL = [groupContainerURLs objectForKey:key];
+                NSLog(@"Group: %@ (%@)\n", groupURL.path, key);
+            }
+        } else {
+            NSLog(@"");
+        }
 
-	    i++;
-	}
-	
+        i++;
+    }
+    
 }
 @end
 
 int main(int argc, char **argv, char **envp) {
 
-	if(argc < 2) {
-		fprintf(stderr, "Syntax: %s searchTerm\n", argv[0]);
-		return 1;
-	}
+    if(argc < 2) {
+        fprintf(stderr, "Syntax: %s searchTerm\n", argv[0]);
+        return 1;
+    }
 
-	char *filter = NULL;
+    char *filter = NULL;
 
   if (optind < argc)
     {
@@ -121,9 +121,9 @@ int main(int argc, char **argv, char **envp) {
         exit(0);
     }
 
-	NSString* searchTerm = [NSString stringWithUTF8String:filter];
+    NSString* searchTerm = [NSString stringWithUTF8String:filter];
     [[AppUtils sharedInstance] searchApp:searchTerm];
 
   exit (0);
-	
+    
 }
